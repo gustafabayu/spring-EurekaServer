576478c06ad5ce576d1deb3a9e8ade5bff83b668
Gustafa Bayu <faafa13@gmail.com>
2023-10-05 09:50:47 +0700

    final project

---
+++ src/test/java/com/api/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
